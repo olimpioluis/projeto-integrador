79fdcf985f9e01ca98db464d918a932b274fbd34
arthur-meli <arthur.souza@mercadolivre.com>
2022-04-22 16:19:13 +0100

    feat: add model InBoundOrder

---
+++ src/main/java/br/com/meli/projetointegrador/model/InboundOrder.java
@@ -0,0 +1,35 @@
+package br.com.meli.projetointegrador.model;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="InboundOrder")
+public class InboundOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalDate orderDate;
+
+    // Um representante vendeu vários lotes
+
+    @ManyToOne(mappedBy = "Batch", cascade = CascadeType.ALL)
+    private List<Batch> itens = new ArrayList<Batch>();
+
+    // Um representante vendeu vários lotes
+    @OneToMany(mappedBy = "StockManager", cascade = CascadeType.ALL)
+    private List<StockManager> itens = new ArrayList<StockManager>();
+
+}
