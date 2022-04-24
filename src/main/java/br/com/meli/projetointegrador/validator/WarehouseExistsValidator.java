47d1c3986f18645e675149d4b59c54165427c1cb
Igor Nogueira <igor.nogueira@mercadolivre.com>
2022-04-24 19:16:28 -0300

    feat: create warehouse exists validator

---
+++ src/main/java/br/com/meli/projetointegrador/validator/WarehouseExistsValidator.java
@@ -0,0 +1,18 @@
+package br.com.meli.projetointegrador.validator;
+
+import br.com.meli.projetointegrador.exception.WarehouseNotExistsException;
+import br.com.meli.projetointegrador.model.Warehouse;
+import br.com.meli.projetointegrador.repository.WarehouseRepository;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class WarehouseExistsValidator implements Validator{
+
+    private Warehouse warehouse;
+    private WarehouseRepository warehouseRepository;
+
+    @Override
+    public void valida() {
+        if (!warehouseRepository.existsById(warehouse.getId())) throw new WarehouseNotExistsException("Este armazem não existe.");
+    }
+}
