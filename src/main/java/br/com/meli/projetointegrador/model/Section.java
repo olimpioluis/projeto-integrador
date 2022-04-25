7c1c7428fb9fa40360e066f26142666b91417a57
Luis Olimpio <luis.olimpio@mercadolivre.com>
2022-04-25 17:57:54 -0300

    feat: create Section model

--- src/main/java/br/com/meli/projetointegrador/model/Section.java
+++ src/main/java/br/com/meli/projetointegrador/model/Section.java
@@ -19,6 +19,8 @@ public class Section {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Enumerated(EnumType.STRING)
     private Category category;
     private Integer size;
 
