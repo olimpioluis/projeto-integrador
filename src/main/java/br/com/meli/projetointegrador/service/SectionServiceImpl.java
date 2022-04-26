130228a35f914f2110555277929730c885e159cb
Luis Olimpio <luis.olimpio@mercadolivre.com>
2022-04-26 16:52:46 -0300

    feat: add implementation of method updateCurrentSize

--- src/main/java/br/com/meli/projetointegrador/service/SectionServiceImpl.java
+++ src/main/java/br/com/meli/projetointegrador/service/SectionServiceImpl.java
@@ -11,8 +11,18 @@ import org.springframework.stereotype.Service;
 public class SectionServiceImpl implements SectionService {
 
     private SectionRepository sectionRepository;
+
     @Override
     public Section findById(Long id) {
         return sectionRepository.findById(id).orElseThrow(() -> new InexistentSectionException("Section " + id + " does not exists!"));
+    }
+
+    @Override
+    public void updateCurrentSize(Integer amountUsed, Long id) {
+        Section section = findById(id);
+
+        section.setCurrentSize(section.getCurrentSize() - amountUsed);
+
+        sectionRepository.save(section);
     }
 }
