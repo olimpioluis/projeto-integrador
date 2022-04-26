8eba6285470059f8e6ab387d87e24c8eaaeae92f
arthur-meli <arthur.souza@mercadolivre.com>
2022-04-26 14:44:16 +0100

    feat: add model Item

---
+++ src/main/java/br/com/meli/projetointegrador/model/Item.java
@@ -0,0 +1,31 @@
+package br.com.meli.projetointegrador.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Advertisement advertisement;
+
+    @ManyToOne
+    private Car car;
+
+    private Integer quantity;
+
+}
