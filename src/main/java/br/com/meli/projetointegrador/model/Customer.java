d7e3e5095c8d64a889274aada2433532ce36b3cc
arthur-meli <arthur.souza@mercadolivre.com>
2022-04-26 14:44:34 +0100

    feat: add model Customer

--- src/main/java/br/com/meli/projetointegrador/model/Customer.java
+++ src/main/java/br/com/meli/projetointegrador/model/Customer.java
@@ -1,4 +1,25 @@
 package br.com.meli.projetointegrador.model;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Person person;
+
+
 }
