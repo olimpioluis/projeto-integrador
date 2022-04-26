f942f516fb30bc86fcd9ccaefa257577c5be8c27
jeferson <jeferson.barbosa@mercadolivre.com>
2022-04-26 11:45:26 -0300

    feat: add method to verify batch using it's section

--- src/main/java/br/com/meli/projetointegrador/repository/BatchRepository.java
+++ src/main/java/br/com/meli/projetointegrador/repository/BatchRepository.java
@@ -4,4 +4,5 @@ import br.com.meli.projetointegrador.model.Batch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BatchRepository extends JpaRepository<Batch, Long> {
+    Batch findBySectionId(Long sectionId);
 }
