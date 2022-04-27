4a461d89b8e11a1c55e55f53387776731565bcc6
Luis Olimpio <luis.olimpio@mercadolivre.com>
2022-04-27 18:10:43 -0300

    feat: add new services and adjusts on existing ones methods

--- src/main/java/br/com/meli/projetointegrador/service/BatchService.java
+++ src/main/java/br/com/meli/projetointegrador/service/BatchService.java
@@ -2,7 +2,11 @@ package br.com.meli.projetointegrador.service;
 
 import br.com.meli.projetointegrador.model.Batch;
 
+import java.util.List;
+
 public interface BatchService {
     void save(Batch[] batches);
     Batch findById(Long id);
+    List<Batch> getBatchesWithExpirationDateGreaterThan3Weeks(Long productId);
+    List<Batch> findAllBatchesByProduct(Long productId);
 }
--- src/main/java/br/com/meli/projetointegrador/service/BatchServiceImpl.java
+++ src/main/java/br/com/meli/projetointegrador/service/BatchServiceImpl.java
@@ -6,7 +6,12 @@ import br.com.meli.projetointegrador.repository.BatchRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -22,5 +27,17 @@ public class BatchServiceImpl implements BatchService {
     @Override
     public Batch findById(Long id) {
         return batchRepository.findById(id).orElseThrow(() -> new InexistentBatchException("Batch " + id + " does not exists!"));
+    }
+
+    @Override
+    public List<Batch> getBatchesWithExpirationDateGreaterThan3Weeks(Long productId) {
+        List<Batch> batches = findAllBatchesByProduct(productId);
+
+        return batches.stream().filter(batch -> ChronoUnit.DAYS.between(LocalDate.now(), batch.getExpirationDate()) > 21).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Batch> findAllBatchesByProduct(Long productId) {
+        return batchRepository.findAllByProductId(productId);
     }
 }
