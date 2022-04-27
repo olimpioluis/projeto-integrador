85d99aabc7ebb051845f686287ea5834ed722693
arthur-meli <arthur.souza@mercadolivre.com>
2022-04-27 14:21:42 +0100

    feat: create authService

---
+++ src/main/java/br/com/meli/projetointegrador/service/AuthService.java
@@ -0,0 +1,129 @@
+package br.com.meli.projetointegrador.service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import br.com.meli.projetointegrador.model.ERole;
+import br.com.meli.projetointegrador.model.Role;
+import br.com.meli.projetointegrador.model.User;
+import br.com.meli.projetointegrador.model.request.LoginRequest;
+import br.com.meli.projetointegrador.model.request.SignupRequest;
+import br.com.meli.projetointegrador.model.response.JwtResponse;
+import br.com.meli.projetointegrador.model.response.MessageResponse;
+import br.com.meli.projetointegrador.repository.RoleRepository;
+import br.com.meli.projetointegrador.repository.UserRepository;
+import br.com.meli.projetointegrador.security.jwt.JwtUtils;
+import br.com.meli.projetointegrador.security.services.UserDetailsImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class AuthService {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+
+
+    public ResponseEntity<?> authenticateUser(LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = new ArrayList<>();
+        for (GrantedAuthority item : userDetails.getAuthorities()) {
+            String authority = item.getAuthority();
+            roles.add(authority);
+        }
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
+    }
+
+
+    public ResponseEntity<?> registerUser(SignupRequest signUpRequest) {
+
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+        if (userRepository.existsByCpf(signUpRequest.getCpf())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: CPF is already in use!"));
+        }
+
+        User user = new User(signUpRequest.getName(),
+                signUpRequest.getCpf(),
+                signUpRequest.getEmail(),
+                signUpRequest.getUsername(),
+                encoder.encode(signUpRequest.getPassword()));
+
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_CUSTOMER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "manager":
+                        Role stockManager = roleRepository.findByName(ERole.ROLE_STOCK_MANAGER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(stockManager);
+
+                        break;
+                    case "seller":
+                        Role sellerRole = roleRepository.findByName(ERole.ROLE_SELLER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(sellerRole);
+
+                        break;
+                    default:
+                        Role customerRole = roleRepository.findByName(ERole.ROLE_CUSTOMER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(customerRole);
+                }
+            });
+        }
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+}
