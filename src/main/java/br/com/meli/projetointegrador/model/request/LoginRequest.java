284fae1b7fc3220ff43be62452a7d43f014c04fe
arthur-meli <arthur.souza@mercadolivre.com>
2022-04-27 14:19:47 +0100

    feat: create login and logout request/response

---
+++ src/main/java/br/com/meli/projetointegrador/model/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package br.com.meli.projetointegrador.model.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+
+
+public class LoginRequest {
+	@NotBlank
+  	private String username;
+
+	@NotBlank
+	private String password;
+
+
+}
---
+++ src/main/java/br/com/meli/projetointegrador/model/request/SignupRequest.java
@@ -0,0 +1,40 @@
+package br.com.meli.projetointegrador.model.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
+public class SignupRequest {
+  @NotBlank
+  @Size(min = 3, max = 20)
+  private String name;
+
+  @NotBlank
+  @Size(min = 3, max = 20)
+  private String username;
+
+  @NotBlank
+  @Size(max = 50)
+  @Email
+  private String email;
+
+  @NotBlank
+  private String cpf;
+
+  private Set<String> role;
+
+  @NotBlank
+  @Size(min = 6, max = 40)
+  private String password;
+
+
+  private Integer warehouse_id;
+
+
+}
+
