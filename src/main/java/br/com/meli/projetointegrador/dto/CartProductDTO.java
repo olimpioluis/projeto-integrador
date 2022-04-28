29ae4651c1bde39128e0e603b6e39789e8dc3b08
Igor Nogueira <igor.nogueira@mercadolivre.com>
2022-04-28 16:43:02 -0300

    refactor: add new field to cart product DTO

--- src/main/java/br/com/meli/projetointegrador/dto/CartProductDTO.java
+++ src/main/java/br/com/meli/projetointegrador/dto/CartProductDTO.java
@@ -4,6 +4,7 @@ import br.com.meli.projetointegrador.model.Item;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,9 +14,10 @@ public class CartProductDTO {
 
     private String name;
     private Integer quantity;
+    private BigDecimal price;
 
     public static CartProductDTO map(Item item) {
-        return new CartProductDTO(item.getAdvertisement().getProduct().getName(), item.getQuantity());
+        return new CartProductDTO(item.getAdvertisement().getProduct().getName(), item.getQuantity(), item.getAdvertisement().getPrice());
     }
 
     public static List<CartProductDTO> map(List<Item> items) {
