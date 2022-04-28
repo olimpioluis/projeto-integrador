9c4b8adfb91e836482d0408fabaaab2816dc3be4
Igor Nogueira <igor.nogueira@mercadolivre.com>
2022-04-28 16:40:42 -0300

    refactor: add new field to cart

--- src/main/java/br/com/meli/projetointegrador/dto/CartDTO.java
+++ src/main/java/br/com/meli/projetointegrador/dto/CartDTO.java
@@ -6,6 +6,11 @@ import br.com.meli.projetointegrador.model.Item;
 import br.com.meli.projetointegrador.model.OrderStatus;
 import br.com.meli.projetointegrador.service.AdvertisementService;
 import br.com.meli.projetointegrador.service.CustomerService;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.Getter;
 
 import java.time.LocalDate;
@@ -15,7 +20,10 @@ import java.util.stream.Collectors;
 @Getter
 public class CartDTO {
 
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate orderDate;
+
     private Long customerId;
     private OrderStatus orderStatus;
     private List<ItemDTO> items;
@@ -26,6 +34,7 @@ public class CartDTO {
 
         return Cart.builder()
                 .customer(customer)
+                .orderDate(cartDTO.getOrderDate())
                 .orderStatus(cartDTO.getOrderStatus())
                 .items(items)
                 .build();
