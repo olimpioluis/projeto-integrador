2e95164439e8553e02e1232a4dbecbaa5cc054a7
Igor Nogueira <igor.nogueira@mercadolivre.com>
2022-04-28 16:36:34 -0300

    refactor: add new attribute

--- src/main/java/br/com/meli/projetointegrador/model/Cart.java
+++ src/main/java/br/com/meli/projetointegrador/model/Cart.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
@@ -17,6 +18,8 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private LocalDate orderDate;
 
     @ManyToOne
     private Customer customer;
