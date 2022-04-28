62d2a79df2218b265f17a736c490dec61b61b22b
Igor Nogueira <igor.nogueira@mercadolivre.com>
2022-04-28 11:06:52 -0300

    refactor: change return from save method

--- src/main/java/br/com/meli/projetointegrador/service/BatchService.java
+++ src/main/java/br/com/meli/projetointegrador/service/BatchService.java
@@ -2,7 +2,9 @@ package br.com.meli.projetointegrador.service;
 
 import br.com.meli.projetointegrador.model.Batch;
 
+import java.util.List;
+
 public interface BatchService {
-    void save(Batch[] batches);
+    List<Batch> save(List<Batch> batches);
     Batch findById(Long id);
 }
