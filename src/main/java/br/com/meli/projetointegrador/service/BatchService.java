740afc1ac24c515e55d1b3cc80fb25b3baee761d
Igor Nogueira <igor.nogueira@mercadolivre.com>
2022-04-28 16:34:52 -0300

    feat: create method to take out products and decrease batch

--- src/main/java/br/com/meli/projetointegrador/service/BatchService.java
+++ src/main/java/br/com/meli/projetointegrador/service/BatchService.java
@@ -1,6 +1,7 @@
 package br.com.meli.projetointegrador.service;
 
 import br.com.meli.projetointegrador.model.Batch;
+import br.com.meli.projetointegrador.model.Item;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface BatchService {
     Batch findById(Long id);
     List<Batch> getBatchesWithExpirationDateGreaterThan3Weeks(Long productId);
     List<Batch> findAllBatchesByProduct(Long productId);
+    void takeOutProducts(List<Item> items);
+    void decreaseBatch(List<Batch> batches, Integer remainingQuantity);
 }
--- src/main/java/br/com/meli/projetointegrador/service/BatchServiceImpl.java
+++ src/main/java/br/com/meli/projetointegrador/service/BatchServiceImpl.java
@@ -2,12 +2,14 @@ package br.com.meli.projetointegrador.service;
 
 import br.com.meli.projetointegrador.exception.InexistentBatchException;
 import br.com.meli.projetointegrador.model.Batch;
+import br.com.meli.projetointegrador.model.Item;
 import br.com.meli.projetointegrador.repository.BatchRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
 public class BatchServiceImpl implements BatchService {
 
     private BatchRepository batchRepository;
+    private SectionService sectionService;
 
     @Override
     public List<Batch> save(List<Batch> batches) {
@@ -39,4 +42,40 @@ public class BatchServiceImpl implements BatchService {
     public List<Batch> findAllBatchesByProduct(Long productId) {
         return batchRepository.findAllByProductId(productId);
     }
+
+    @Override
+    public void decreaseBatch(List<Batch> batches, Integer remainingQuantity) {
+
+        for (Batch batch:batches){
+
+            if (remainingQuantity == 0) break;
+
+            else if(batch.getCurrentQuantity().equals(remainingQuantity)){
+                remainingQuantity = 0;
+                batch.setCurrentQuantity(0);
+                sectionService.updateCurrentSize(1, batch.getSection().getId(), true);
+            }
+            else if(batch.getCurrentQuantity() > remainingQuantity){
+                batch.setCurrentQuantity(batch.getCurrentQuantity() - remainingQuantity);
+                remainingQuantity = 0;
+            }
+            else{
+                remainingQuantity -= batch.getCurrentQuantity();
+                batch.setCurrentQuantity(0);
+                sectionService.updateCurrentSize(1, batch.getSection().getId(), true);
+            }
+            batchRepository.save(batch);
+        }
+    }
+
+    @Override
+    public void takeOutProducts(List<Item> items) {
+        items.forEach(item -> {
+            List<Batch> batches = getBatchesWithExpirationDateGreaterThan3Weeks(item.getAdvertisement().getId());
+            batches.sort(Comparator.comparing(Batch::getCurrentQuantity));
+            decreaseBatch(batches, item.getQuantity());
+        });
+    }
+
+
 }
