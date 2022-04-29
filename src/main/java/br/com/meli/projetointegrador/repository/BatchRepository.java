afe2cf1175af8610fcf953894471406b915bbcbf
jeferson <jeferson.barbosa@mercadolivre.com>
2022-04-29 18:03:28 -0300

    add: Add three queries to filter, since H2 has trouble in integration tests

--- src/main/java/br/com/meli/projetointegrador/repository/BatchRepository.java
+++ src/main/java/br/com/meli/projetointegrador/repository/BatchRepository.java
@@ -5,13 +5,23 @@ import br.com.meli.projetointegrador.model.Batch;
 import br.com.meli.projetointegrador.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BatchRepository extends JpaRepository<Batch, Long> {
     Batch findBySectionId(Long sectionId);
 
-    @Query(value = "select p.*, b.id as batchId, b.expiration_date as expirationDate, b.current_quantity as currentQuantity from batch as b , (select * from product where id=?1 ) p where b.product_id=p.id order by ?2 DESC", nativeQuery = true)
-    List<ProductByBatchResponse> getAllProductThatHaveBatch(Long id, String orderBy);
+    @Query(value = "select p.*, b.id as batchId, b.expiration_date as expirationDate, b.current_quantity as " +
+            "currentQuantity from batch as b , (select * from product where id=?1 ) p where b.product_id=p.id order by batchId DESC", nativeQuery = true)
+    List<ProductByBatchResponse> getAllProductThatHaveBatch(Long id);
+
+    @Query(value = "select p.*, b.id as batchId, b.expiration_date as expirationDate, b.current_quantity as " +
+            "currentQuantity from batch as b , (select * from product where id=?1 ) p where b.product_id=p.id order by expirationDate DESC", nativeQuery = true)
+    List<ProductByBatchResponse> getAllProductThatHaveBatchExpiration(Long id);
+
+    @Query(value = "select p.*, b.id as batchId, b.expiration_date as expirationDate, b.current_quantity as " +
+            "currentQuantity from batch as b , (select * from product where id=?1 ) p where b.product_id=p.id order by currentQuantity DESC", nativeQuery = true)
+    List<ProductByBatchResponse> getAllProductThatHaveBatchQuantity(Long id);
 
 }
