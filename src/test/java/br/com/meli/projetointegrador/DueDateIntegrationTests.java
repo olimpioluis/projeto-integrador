6c91db985727fdd2bb1a56e7d238080d8c97465d
Jederson Carvalho Macedo <jederson.macedo@mercadolivre.com>
2022-04-29 16:50:23 -0300

    fix: fix InboundOrderIntegrationTest

--- src/test/java/br/com/meli/projetointegrador/DueDateIntegrationTests.java
+++ src/test/java/br/com/meli/projetointegrador/DueDateIntegrationTests.java
@@ -34,14 +34,14 @@ public class DueDateIntegrationTests {
                 "    \"orderNumber\": 10,\n" +
                 "    \"orderDate\": \"2022-04-27\",\n" +
                 "    \"section\": {\n" +
-                "        \"sectionCode\": 3,\n" +
+                "        \"sectionCode\": 1,\n" +
                 "        \"warehouseCode\": 1\n" +
                 "    },\n" +
                 "    \"batchStock\": [\n" +
                 "        {\n" +
                 "            \"productId\": 1,\n" +
                 "            \"currentTemperature\": 25.5,\n" +
-                "            \"minTemperature\": 10.5,\n" +
+                "            \"minTemperature\": -10.5,\n" +
                 "            \"initialQuantity\": 20,\n" +
                 "            \"currentQuantity\": 20,\n" +
                 "            \"manufacturingDate\": \"2022-01-01\",\n" +
@@ -51,7 +51,7 @@ public class DueDateIntegrationTests {
                 "        {\n" +
                 "            \"productId\": 2,\n" +
                 "            \"currentTemperature\": 25.5,\n" +
-                "            \"minTemperature\": 10.5,\n" +
+                "            \"minTemperature\": -10.5,\n" +
                 "            \"initialQuantity\": 20,\n" +
                 "            \"currentQuantity\": 20,\n" +
                 "            \"manufacturingDate\": \"2022-01-01\",\n" +
@@ -61,7 +61,7 @@ public class DueDateIntegrationTests {
                 "        {\n" +
                 "            \"productId\": 3,\n" +
                 "            \"currentTemperature\": 25.5,\n" +
-                "            \"minTemperature\": 10.5,\n" +
+                "            \"minTemperature\": -10.5,\n" +
                 "            \"initialQuantity\": 20,\n" +
                 "            \"currentQuantity\": 20,\n" +
                 "            \"manufacturingDate\": \"2022-01-01\",\n" +
@@ -71,7 +71,7 @@ public class DueDateIntegrationTests {
                 "        {\n" +
                 "            \"productId\": 3,\n" +
                 "            \"currentTemperature\": 25.5,\n" +
-                "            \"minTemperature\": 10.5,\n" +
+                "            \"minTemperature\": -10.5,\n" +
                 "            \"initialQuantity\": 20,\n" +
                 "            \"currentQuantity\": 20,\n" +
                 "            \"manufacturingDate\": \"2022-01-01\",\n" +
@@ -113,7 +113,7 @@ public class DueDateIntegrationTests {
 
         postInboundOrder(inboundOrderDTO, status().isCreated());
 
-        String checkBatchStockDueDate = getCheckBatchStockDueDate("64", "3",status().isOk());
+        String checkBatchStockDueDate = getCheckBatchStockDueDate("64", "1",status().isOk());
 
         List<BatchStockDueDateDTO> batchStockDueDateDTOList = objectMapper.readValue(checkBatchStockDueDate, new TypeReference<>() {});
 
@@ -137,8 +137,8 @@ public class DueDateIntegrationTests {
     @Test
     void getBatchStockByDueDateWithCategory() throws Exception {
 
-        String checkBatchStockDueDateWithCategory1 = getCheckBatchStockDueDateWithCategory("95", "FRESH", "asc", status().isOk());
-        String checkBatchStockDueDateWithCategory2 = getCheckBatchStockDueDateWithCategory("95", "FRESH", "desc", status().isOk());
+        String checkBatchStockDueDateWithCategory1 = getCheckBatchStockDueDateWithCategory("95", "FROZEN", "asc", status().isOk());
+        String checkBatchStockDueDateWithCategory2 = getCheckBatchStockDueDateWithCategory("95", "FROZEN", "desc", status().isOk());
 
         List<BatchStockDueDateDTO> batchStockDueDateDTOList1 = objectMapper.readValue(checkBatchStockDueDateWithCategory1, new TypeReference<>() {});
         List<BatchStockDueDateDTO> batchStockDueDateDTOList2 = objectMapper.readValue(checkBatchStockDueDateWithCategory2, new TypeReference<>() {});
