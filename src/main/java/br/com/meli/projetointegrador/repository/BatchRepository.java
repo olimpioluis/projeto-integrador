7ea595e6178b11580c7b77655a3cc97b968402ad
jeferson <jeferson.barbosa@mercadolivre.com>
2022-04-29 12:53:25 -0300

    feat: Add query to select products and order by

--- src/main/java/br/com/meli/projetointegrador/repository/BatchRepository.java
+++ src/main/java/br/com/meli/projetointegrador/repository/BatchRepository.java
@@ -1,8 +1,17 @@
 package br.com.meli.projetointegrador.repository;
 
+import br.com.meli.projetointegrador.dto.ProductByBatchResponse;
 import br.com.meli.projetointegrador.model.Batch;
+import br.com.meli.projetointegrador.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BatchRepository extends JpaRepository<Batch, Long> {
     Batch findBySectionId(Long sectionId);
+
+    @Query(value = "select p.*, b.id as batchId, b.expiration_date as expirationDate, b.current_quantity as currentQuantity from batch as b , (select * from product where id=?1 ) p where b.product_id=p.id order by ?2 DESC", nativeQuery = true)
+    List<ProductByBatchResponse> getAllProductThatHaveBatch(Long id, String orderBy);
+
 }
