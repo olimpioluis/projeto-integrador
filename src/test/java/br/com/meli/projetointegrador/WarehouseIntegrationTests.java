a1b3c0aff78840dd5edf8cead7ebbc40dc5497a0
jeferson <jeferson.barbosa@mercadolivre.com>
2022-05-02 15:09:32 -0300

    fix: add disty context annotation

--- src/test/java/br/com/meli/projetointegrador/WarehouseIntegrationTests.java
+++ src/test/java/br/com/meli/projetointegrador/WarehouseIntegrationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 public class WarehouseIntegrationTests {
 
     @Autowired
